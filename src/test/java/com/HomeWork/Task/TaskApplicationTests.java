117d67fc8d29823d0009cc7d989af0aa4c26121d
anatolii <anatoliise@mail.ru>
2024-02-24 00:45:46 +0300

    comit_task

---
+++ src/test/java/com/HomeWork/Task/TaskApplicationTests.java
@@ -0,0 +1,15 @@
+package com.HomeWork.Task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
