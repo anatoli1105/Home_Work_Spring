bee21aba687c93648913487faffdd0d08862564c
anatolii <anatoliise@mail.ru>
2024-02-24 01:14:11 +0300

    comit_task

---
+++ src/test/java/com/HomeWork/Task/Servis.java
@@ -0,0 +1,23 @@
+package com.HomeWork.Task;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class Servis {
+    private final Basket basket = new Basket();
+
+    public Servis() {
+    }
+
+    public void add(int value) {
+
+        this.basket.add(value);
+    }
+
+    public Collection<Integer> get() {
+
+        return this.basket.get();
+    }
+}
--- src/test/java/com/HomeWork/Task/TaskApplicationTests.java
+++ src/test/java/com/HomeWork/Task/TaskApplicationTests.java
@@ -1,12 +1,17 @@
 package com.HomeWork.Task;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class TaskApplicationTests {
-	@Test
-	void contextLoads() {
+
+@SpringBootApplication
+public class TaskApplicationTests {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskApplicationTests.class, args);
 	}
 
 }
